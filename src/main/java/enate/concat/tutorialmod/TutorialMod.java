a754a9ae6b5fd8872c0c7b989abcc0ad58df3e3b
concat-enate <soylesssoymilk@gmail.com>
2024-10-26 20:16:21 +0100

    added pink garnet, raw and normal

--- src/main/java/enate/concat/tutorialmod/TutorialMod.java
+++ src/main/java/enate/concat/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package enate.concat.tutorialmod;
 
+import enate.concat.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,5 +19,7 @@ public class TutorialMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		ModItems.registerModItems();
 	}
 }
